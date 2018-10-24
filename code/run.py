1c09fd9bf7e38e26da5de6f432ebbec3e1d13fad
Romain Buchs <romainbuchs@hotmail.com>
2018-10-24 11:52:51 +0200

    reproduce paper result

---
+++ code/run.py
@@ -0,0 +1,23 @@
+#Import the relevant modules
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+import yaml
+
+import OpinionGraph
+import OpinionAlgorithm
+
+
+def main():
+
+       
+
+
+if __name__ == '__main__':
+    
+    #cfgfile = sys.argv[1]
+    #with open(cfgfile, 'r') as fp:
+    #    cfg = yaml.load(fp)
+    
+    
+    main()
